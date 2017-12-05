a061740f9d55e9453eddbdcc6fcbac9abeedb600
Michael Andrews <michael@michael-laptop.andrews.fam>
2017-12-04 19:16:00 -0500

    Initial code for WPILib command robot

---
+++ src/org/usfirst/frc/team281/robot/Robot.java
@@ -0,0 +1,58 @@
+
+package org.usfirst.frc.team281.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import org.usfirst.frc.team281.robot.subsystems.DriveSubsystem;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+
+	public static final DriveSubsystem _driveSubsystem = new DriveSubsystem();
+	public static OI oi;
+
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		oi = new OI();
+	}
+
+	/**
+	 * This function is called once each time the robot enters Disabled mode.
+	 * You can use it to reset any subsystem information you want to clear when
+	 * the robot is disabled.
+	 */
+	@Override
+	public void disabledInit() {
+
+	}
+
+	@Override
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
+	public void teleopInit() {
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	@Override
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
+}
---
+++ src/org/usfirst/frc/team281/robot/RobotMap.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team281.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+	// CAN ids (must be unique)
+	public static int frontLeftMotorCANid = 0;
+	public static int frontRightMotorCANid = 2;
+	public static int rearLeftMotorCANid = 1;
+	public static int rearRightMotorCANid = 3;
+
+	public static int driveJoystick = 1;
+}
