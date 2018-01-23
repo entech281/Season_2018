0946229a41eb77538e5d5ac6d76b0e63d0e101ea
Peter Labadorf <plaba3.1415@gmail.com>
2018-01-23 18:33:54 -0500

    ballshoot command completed

---
+++ src/main/java/frc/team281/robot/commands/CompleteBallShoot.java
@@ -0,0 +1,55 @@
+package frc.team281.robot.commands;
+import edu.wpi.first.wpilibj.command.Command;
+
+
+import frc.team281.robot.subsystems.*;
+public class CompleteBallShoot extends Command {
+	private ProngsSubsystem Prongs;
+	private ShooterInTakeSubsystem SIntake;
+	private ShooterOutTakeSubsystem SOutake;
+	public CompleteBallShoot(ProngsSubsystem Prongs,ShooterInTakeSubsystem SIntake,ShooterOutTakeSubsystem SOutake) {
+		this.Prongs  = Prongs;
+		this.SIntake = SIntake;
+		this.SOutake = SOutake;
+		requires(Prongs);
+		requires(SIntake);
+		requires(SOutake);
+	}
+	protected void initialize() {
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		Prongs.lower();
+		SIntake.shooterIn();
+		for(int i=0;i<500;i++) {
+			SOutake.shooterOutBelow();
+		}
+		for(int i=0;i<500;i++) {
+			SOutake.shooterOutAbove();
+		}
+		
+		SOutake.stop();
+	}
+
+	
+	
+
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+	}
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
