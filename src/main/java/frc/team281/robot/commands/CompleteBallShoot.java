d00d5d77d91fe1c00d3b2ac2b903b6ba93b9ddd4
Peter Labadorf <plaba3.1415@gmail.com>
2018-01-25 18:15:24 -0500

    fixed bug

--- src/main/java/frc/team281/robot/commands/CompleteBallShoot.java
+++ src/main/java/frc/team281/robot/commands/CompleteBallShoot.java
@@ -22,12 +22,13 @@ public class CompleteBallShoot extends Command {
 	protected void execute() {
 		Prongs.lower();
 		SIntake.shooterIn();
-		for(int i=0;i<RobotMap.timeUp;i++) {
-			SOutake.shooterOutSLOW();
-		}
 		for(int i=0;i<RobotMap.timeDown;i++) {
 			SOutake.shooterOutAbove();
 		}
+		for(int i=0;i<RobotMap.timeUp;i++) {
+			SOutake.shooterOutSLOW();
+		}
+		
 		SIntake.stop();
 		SOutake.stop();
 	}
