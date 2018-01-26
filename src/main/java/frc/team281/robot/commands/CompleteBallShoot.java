a22881ad955d895199f38849c7a21db55dadcff3
Peter Labadorf <plaba3.1415@gmail.com>
2018-01-25 19:47:59 -0500

    debug

--- src/main/java/frc/team281/robot/commands/CompleteBallShoot.java
+++ src/main/java/frc/team281/robot/commands/CompleteBallShoot.java
@@ -5,8 +5,9 @@ import frc.team281.robot.Robot;
 import frc.team281.robot.commands.*;
 public class CompleteBallShoot extends CommandGroup {
 	public CompleteBallShoot() {
+		addParallel(new ShooterInTake(Robot.m_ShooterInTakeSubsystem));
 		addSequential(new ProngsDown(Robot._prongsSubsystem));
-		addParallel(new ShooterOutTakeBelow(Robot.m_ShooterOutTakeSubsystem));
+		addSequential(new ShooterOutTakeBelow(Robot.m_ShooterOutTakeSubsystem));
 		addSequential(new ShooterOutTakeAbove(Robot.m_ShooterOutTakeSubsystem));
 	}
 }
