ed87b81efc389f19b09e0389248115bfea609040
Peter Labadorf <plaba3.1415@gmail.com>
2018-02-01 18:47:24 -0500

    updated dot move.

--- PowerUProbot2018/src/main/java/frc/team281/robot/commands/DotMoveCommand.java
+++ PowerUProbot2018/src/main/java/frc/team281/robot/commands/DotMoveCommand.java
@@ -1,11 +1,19 @@
 package frc.team281.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.team281.robot.subsystems.DriveSubsystem;
-public class DotMoveCommand<x,y> extends Command{
+import frc.team281.robot.Robot;
+public class DotMoveCommand extends Command{
 	private DriveSubsystem drive;
-	public DotMoveCommand(DriveSubsystem d) {
+	private double x;
+	private double y;
+	public DotMoveCommand(DriveSubsystem d,double x,double y) {
 		this.drive=d;
+		this.x=x;
+		this.y=y;
 		requires(d);
+	}
+	private double CalculateAngle() {
+		return Math.atan2(y-Robot.y, x-Robot.x);
 	}
 	
 	@Override
