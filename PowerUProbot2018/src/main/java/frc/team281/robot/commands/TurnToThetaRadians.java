d0cebefdea8f11fa8f3050fa37ceff7307816378
Peter Labadorf <plaba3.1415@gmail.com>
2018-02-09 17:13:39 -0500

    added code.

--- PowerUProbot2018/src/main/java/frc/team281/robot/commands/TurnToThetaRadians.java
+++ PowerUProbot2018/src/main/java/frc/team281/robot/commands/TurnToThetaRadians.java
@@ -1,5 +1,4 @@
 package frc.team281.robot.commands;
-
 import frc.team281.robot.Robot;
 import frc.team281.robot.subsystems.DriveSubsystem;
 
@@ -7,36 +6,30 @@ public class TurnToThetaRadians extends BaseCommand {
 	private DriveSubsystem drive;
 	private double x;
 	public TurnToThetaRadians(DriveSubsystem drive, double theta) {
-		// Use requires() here to declare subsystem dependencies
 		this.drive = drive;
 		this.x=theta;
 		requires(drive);
+		setTimeout(x);
 	}
 
-	// Called just before this Command runs the first time
 	@Override
 	protected void initialize() {
 	}
 
-	// Called repeatedly when this Command is scheduled to run
 	@Override
 	protected void execute() {
 		drive.arcadeDrive(0, 1);
 	}
 
-	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	protected boolean isFinished() {
-		return false;
+		return isTimedOut();
 	}
 
-	// Called once after isFinished returns true
 	@Override
 	protected void end() {
 	}
 
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
 	@Override
 	protected void interrupted() {
 	}
