a7a9dbbf135f2d427a912fe1d43f9283211a0787
Naveen <Naveen-MBA@Naveens-MacBook-Air.local>
2018-02-18 08:44:32 -0500

    added couple tests

---
+++ Macarena2018/src/main/java/frc/team281/robot/ConvertFieldMessageToCommandGroup.java
@@ -0,0 +1,10 @@
+package frc.team281.robot;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class ConvertFieldMessageToCommandGroup {
+	public CommandGroup convert() {
+		return null;
+		
+	}
+}
