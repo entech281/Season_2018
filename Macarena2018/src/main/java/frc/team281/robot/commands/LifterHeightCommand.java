a398ee2ffffcf337ccdff51b3f8fcad42cfee3ce
Dave <dave.cowden@gmail.com>
2018-02-18 18:05:24 -0500

    add lifter commands

--- Macarena2018/src/main/java/frc/team281/robot/commands/LifterHeightCommand.java
+++ Macarena2018/src/main/java/frc/team281/robot/commands/LifterHeightCommand.java
@@ -1,23 +1,57 @@
 package frc.team281.robot.commands;
 
 import frc.team281.robot.subsystems.BaseSubsystem;
+import frc.team281.robot.subsystems.LifterSubsystem;
+
 
 public class LifterHeightCommand extends BaseCommand {
 
-    public LifterHeightCommand(BaseSubsystem subsystem) {
-        super(subsystem);
-        // TODO Auto-generated constructor stub
+    public static final int TIMEOUT_SECS = 20;
+    public static final double HEIGHT_TOLERANCE=1.0;
+    
+    private LifterSubsystem lifter;
+    
+    private double desiredHeightInches = 0.0;
+    public LifterHeightCommand(BaseSubsystem subsystem, double heightInches) {
+        super(subsystem, TIMEOUT_SECS);
+        this.lifter = (LifterSubsystem)subsystem;
+       
+        this.desiredHeightInches = lifter.trimHeight(heightInches);
+    }
+    
+    @Override
+    protected void initialize() {
+        double currentHeightInches = lifter.getHeightInches();
+        if ( currentHeightInches < desiredHeightInches){            
+            lifter.motorsUp(LifterSubsystem.FULL_SPEED_PERCENT);
+        }
+        else{
+            lifter.motorsDown(LifterSubsystem.FULL_SPEED_PERCENT);
+        }        
     }
 
-    public LifterHeightCommand(BaseSubsystem subsystem, double timeOut) {
-        super(subsystem, timeOut);
-        // TODO Auto-generated constructor stub
+    @Override
+    protected void end() {
+        lifter.motorsOff();
     }
 
     @Override
     protected boolean isFinished() {
-        // TODO Auto-generated method stub
-        return false;
+        boolean isAtRequestedHeight = isHeightAtRequestedHeight();
+        dataLogger.log("At Reqeusted Height",isAtRequestedHeight);
+        return isAtRequestedHeight || isTimedOut();
+    }
+    
+    protected boolean isHeightAtRequestedHeight(){
+        double currentHeightInches= lifter.getHeightInches();
+        dataLogger.log("RequestedHeight",desiredHeightInches);
+        dataLogger.log("CurrentHeight",currentHeightInches);
+        return Math.abs(currentHeightInches - desiredHeightInches) < HEIGHT_TOLERANCE;
+    }
+    
+    @Override
+    protected void interrupted() {
+        end();
     }
 
 }
---
+++ Macarena2018/src/main/java/frc/team281/robot/commands/LifterHomeCommand.java
@@ -0,0 +1,38 @@
+package frc.team281.robot.commands;
+
+import frc.team281.robot.subsystems.LifterSubsystem;
+
+public class LifterHomeCommand extends BaseCommand {
+
+    public static final int TIMEOUT_SECS = 20;
+    
+    private LifterSubsystem lifter;
+    
+    public LifterHomeCommand(LifterSubsystem subsystem) {
+        super(subsystem,TIMEOUT_SECS);
+        this.lifter = (LifterSubsystem)subsystem;
+    }
+
+    @Override
+    protected void initialize() {
+        lifter.motorsDown(LifterSubsystem.HOMING_SPEED_PERCENT);
+    }
+
+    @Override
+    protected void end() {
+        lifter.motorsOff();
+        if ( lifter.isLifterAtBottom()){
+            lifter.setZeroPosition();
+        }
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return lifter.isLifterAtBottom() || isTimedOut();
+    }
+    
+    @Override
+    protected void interrupted() {
+        end();
+    }
+}
