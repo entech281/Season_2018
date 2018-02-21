f2781df864f1db9f60d86f0dd1cfb5e40df66cf2
Peter Labadorf <plaba3.1415@gmail.com>
2018-02-20 20:53:16 -0500

    final debug (I hope)

--- Macarena2018/src/main/java/frc/team281/robot/subsystems/PositionCalculator.java
+++ Macarena2018/src/main/java/frc/team281/robot/subsystems/PositionCalculator.java
@@ -3,95 +3,86 @@ package frc.team281.robot.subsystems;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class PositionCalculator {
-    
+
     public static final double DISTANCE_BETWEEN_WHEELS = 27.5;
-    
+
     private static double computeTurn(double degrees) {
-        return (((DISTANCE_BETWEEN_WHEELS*Math.PI)/360)*degrees);
-        
+        return (((DISTANCE_BETWEEN_WHEELS * Math.PI) / 360) * degrees);
+
     }
-    
+
     public static Position goForward(double inches) {
-        double desiredPosition = inches;        
+        double desiredPosition = inches;
         return new Position(desiredPosition, desiredPosition);
     }
-    
+
     public static Position turnRight(double degrees) {
-    	double d = computeTurn(degrees);
-    	return new Position( d,-d);
+        double d = computeTurn(degrees);
+        return new Position(d, -d);
     }
-    
-    
+
     public static Position turnLeft(double degrees) {
-    	double d = computeTurn(degrees);
-    	return new Position( -d,d);
+        double d = computeTurn(degrees);
+        return new Position(-d, d);
     }
-    
+
     public static BasicMoves builder() {
-    	return new Builder();
-    }
-    
-    public interface BasicMoves{
-    	BasicMoves right(double degrees);
-    	BasicMoves left(double degrees);
-    	BasicMoves forward(double inches);
-    	BasicMoves backward(double inches);
-    	BasicMoves mirror();
-    	List<Position> build();
+        return new Builder();
     }
 
-    public static class Builder implements BasicMoves{
-    	private List<Position> commands = new ArrayList<>();
+    public interface BasicMoves {
+        BasicMoves right(double degrees);
 
-		@Override
-		public List<Position> build() {
-			return commands;
-		}
+        BasicMoves left(double degrees);
 
-		@Override
-		public BasicMoves right(double degrees) {
-			commands.add(turnRight(degrees));
-			return this;
-		}
+        BasicMoves forward(double inches);
 
-		@Override
-		public BasicMoves left(double degrees) {
-			commands.add(turnLeft(degrees));
-			return this;
-		}
+        BasicMoves backward(double inches);
 
-		@Override
-		public BasicMoves forward(double inches) {
-			commands.add(goForward(inches));
-			return this;
-		}
+        List<Position> build();
+    }
 
-		@Override
-		public BasicMoves backward(double inches) {
-			commands.add(goForward(-inches));
-			return this;
-		}
+    public static List<Position> mirror(List<Position> a) {
+        List<Position> b = new ArrayList<Position>();
+        for (int i = 0; i < a.size(); i++) {
+            b.add(new Position(a.get(i).getRightInches(), a.get(i).getLeftInches()));
+        }
+        return b;
+    }
+
+    public static class Builder implements BasicMoves {
+        private List<Position> commands = new ArrayList<>();
 
         @Override
-        public BasicMoves mirror() {
-            BasicMoves b = new Builder();
-            for(int i=0;i<commands.size();i++) {
-                if(commands.get(i).getLeftInches()>0 && commands.get(i).getRightInches()>0) {
-                    b.forward(commands.get(i).getLeftInches());
-                }else if(commands.get(i).getLeftInches()<0 && commands.get(i).getRightInches()<0) {
-                    b.backward(commands.get(i).getLeftInches());
-                }else if(commands.get(i).getLeftInches()<0 && commands.get(i).getRightInches()>0) {
-                    b.left(commands.get(i).getLeftInches());
-                }else {
-                    b.right(commands.get(i).getLeftInches());
-                }
-            }
-            return b;
+        public List<Position> build() {
+            return commands;
         }
-    	    	
-    }	
-	
-    
+
+        @Override
+        public BasicMoves right(double degrees) {
+            commands.add(turnRight(degrees));
+            return this;
+        }
+
+        @Override
+        public BasicMoves left(double degrees) {
+            commands.add(turnLeft(degrees));
+            return this;
+        }
+
+        @Override
+        public BasicMoves forward(double inches) {
+            commands.add(goForward(inches));
+            return this;
+        }
+
+        @Override
+        public BasicMoves backward(double inches) {
+            commands.add(goForward(-inches));
+            return this;
+        }
+
+    }
+
 }
