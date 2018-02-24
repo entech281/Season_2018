fd7c8aa2d74fadad90c2940bbaf0d2d7497116c2
Dave <dave.cowden@gmail.com>
2018-02-24 12:36:32 -0500

    added MotionProfileTankDrive

---
+++ Macarena2018/src/main/java/frc/team281/robot/controllers/MotionProfileTankDrive.java
@@ -0,0 +1,29 @@
+package frc.team281.robot.controllers;
+
+public class MotionProfileTankDrive {
+    
+    public TalonWithSettings leftFront;
+    public TalonWithSettings leftRear;
+    public TalonWithSettings rightFront;
+    public TalonWithSettings rightRear;
+    
+    public void configureAll() {
+        leftFront.configure();
+        leftRear.configure();
+        rightFront.configure();
+        rightRear.configure();
+    }
+    
+    public WPI_Talon getLeftFront() {
+        return leftFront.getTalon();
+    }
+    public WPI_Talon getRightFront() {
+        return leftFront.getTalon();
+    }
+    public WPI_Talon getLeftRear() {
+        return leftFront.getTalon();
+    }
+    public WPI_Talon getLeftRear() {
+        return leftFront.getTalon();
+    }   
+}
