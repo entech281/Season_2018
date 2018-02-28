f3432b7ed735e6054ea09374d4e3e7862f2f3ebc
Michael Andrews <mandrews@mandrews.org>
2018-02-27 19:32:01 -0500

    Use the new operator panel buttons

--- Macarena2018/src/main/java/frc/team281/robot/RobotMap.java
+++ Macarena2018/src/main/java/frc/team281/robot/RobotMap.java
@@ -34,27 +34,21 @@ public class RobotMap {
     public static class DriveJoystick {
         public static final int PORT = 0;
         public static class Buttons {   
-            public static final int LIFTER_RAISE = 5;
-            public static final int LIFTER_LOWER = 3;
-            public static final int LIFTER_SCALE_HIGH = 8;
-            public static final int LIFTER_SCALE_MID = 9;
-            public static final int LIFTER_SCALE_LOW = 10;
-            public static final int LIFTER_SCALE_FENCE = 11;
-            public static final int LIFTER_SCALE_GROUND = 12;
+            public static final int FIELD_ABSOLUTE_DRIVING = 11;
         }
     }
     
     public static class ControlPanel {
         public static final int PORT = 1;
         public static class Buttons {
-            public static final int LIFTER_RAISE = 9;
-            public static final int LIFTER_LOWER =10;
-            public static final int LIFTER_STOP = 11;
-            public static final int GRABBER_LOAD = 7;
-            public static final int GRABBER_SHOOT = 8;
-            public static final int GRABBER_STOP = 9;
-            public static final int GRABBER_OPEN = 10;
-            public static final int GRABBER_CLOSE = 11;
+            public static final int LIFTER_RAISE = 4;
+            public static final int LIFTER_LOWER = 5;
+            public static final int LIFTER_TO_TOP = 3;
+            public static final int LIFTER_TO_GROUND = 2;
+            public static final int GRABBER_LOAD = 8;
+            public static final int GRABBER_SHOOT = 9;
+            public static final int GRABBER_OPEN = 7;
+            public static final int WRIST_UP = 6;
         }
     }
 
