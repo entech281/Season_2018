4a8dacb2c271324a0e6ea74856641089a01e27a5
Michael Andrews <mandrews@mandrews.org>
2018-02-27 19:12:02 -0500

    Add LifterTopCommand and turn motors off in LifterHome

---
+++ Macarena2018/src/main/java/frc/team281/robot/commands/LifterTopCommand.java
@@ -0,0 +1,35 @@
+package frc.team281.robot.commands;
+
+import frc.team281.robot.subsystems.LifterSubsystem;
+
+public class LifterTopCommand extends BaseCommand {
+
+	public static final int TIMEOUT_SECS = 20;
+
+	private LifterSubsystem lifter;
+
+	public LifterTopCommand(LifterSubsystem subsystem) {
+		super(subsystem, TIMEOUT_SECS);
+		this.lifter = (LifterSubsystem) subsystem;
+	}
+
+	@Override
+	protected void initialize() {
+		lifter.motorsUp(LifterSubsystem.FULL_SPEED_PERCENT);
+	}
+
+	@Override
+	protected void end() {
+		lifter.motorsOff();
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return lifter.isLifterAtTop() || isTimedOut();
+	}
+
+	@Override
+	protected void interrupted() {
+		end();
+	}
+}
