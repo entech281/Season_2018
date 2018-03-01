ebc7df7d64fdbb3990833ec9d977530e65b0e9d6
Michael Andrews <mandrews@mandrews.org>
2018-02-28 21:57:37 -0500

    Back to two solenoids like 2017 prongs

--- Macarena2018/src/main/java/frc/team281/robot/subsystems/WristSubsystem.java
+++ Macarena2018/src/main/java/frc/team281/robot/subsystems/WristSubsystem.java
@@ -8,36 +8,46 @@ import frc.team281.robot.RobotMap.PCM;
 
 public class WristSubsystem extends BaseSubsystem {
 
-    //private Solenoid up;
-    //private Solenoid down;
+    private Solenoid up;
+    private Solenoid down;
 
-    private DoubleSolenoid solenoid;
-    private DigitalInput limitSwitch;
-    
+    //2 private DoubleSolenoid solenoid;
+
+    private boolean wristUp;
+
     public WristSubsystem() {
     }
 
     @Override
     public void initialize() {
-        solenoid = new DoubleSolenoid(RobotMap.CAN.PC_MODULE, PCM.Wrist.UP, PCM.Wrist.DOWN);
-        //up = new Solenoid(RobotMap.CAN.PC_MODULE, PCM.Wrist.UP);
-        //down = new Solenoid(RobotMap.CAN.PC_MODULE,PCM.Wrist.DOWN);
+        //2 solenoid = new DoubleSolenoid(RobotMap.CAN.PC_MODULE, PCM.Wrist.UP, PCM.Wrist.DOWN);
+        wristUp = true;
+        up = new Solenoid(RobotMap.CAN.PC_MODULE, PCM.Wrist.UP);
+        down = new Solenoid(RobotMap.CAN.PC_MODULE,PCM.Wrist.DOWN);
 
+    }
+
+    @Override
+    public void periodic() {
+        //2 if (wristUp) {
+        //2     solenoid.set(DoubleSolenoid.Value.kReverse);
+        //2 } else {
+        //2     solenoid.set(DoubleSolenoid.Value.kForward);
+        //2 }
     }
 
     public void pivotUp() {
-        solenoid.set(DoubleSolenoid.Value.kReverse);
-        //up.set(true);
-        //down.set(false);
+        wristUp = true;
+        up.set(true);
+        down.set(true);
     }
     
     public void pivotDown() {
-        solenoid.set(DoubleSolenoid.Value.kForward);
-        //up.set(false);
-        //down.set(true);
+        wristUp = false;
+        up.set(false);
+        down.set(false);
     }
     public boolean wristAtBottom() {
         return limitSwitch.get();
     }
-    
 }
