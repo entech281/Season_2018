ddadd38b64795fdc41597f6f653014b8f6a646e6
Peter Labadorf <plaba3.1415@gmail.com>
2018-03-01 16:20:11 -0500

    added commands.

---
+++ Macarena2018/src/main/java/frc/team281/robot/commands/LifterRaiseSeconds.java
@@ -0,0 +1,25 @@
+package frc.team281.robot.commands;
+
+import frc.team281.robot.subsystems.LifterSubsystem;
+
+public class LifterRaiseSeconds extends BaseCommand {
+
+    private LifterSubsystem lifter;
+    public LifterRaiseSeconds(LifterSubsystem subsystem, double seconds) {
+        super(subsystem);
+        setTimeout(seconds);
+        this.lifter = subsystem;
+    }
+
+    @Override
+    protected void initialize() {
+        lifter.motorsDown(100);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        
+        return isTimedOut();
+    }
+
+}
