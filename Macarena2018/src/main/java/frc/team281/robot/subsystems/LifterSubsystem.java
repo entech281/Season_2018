95f56f991c320a3681447cbb3e0bbdd6b5a0b39d
Michael Andrews <mandrews@mandrews.org>
2018-03-23 17:23:05 -0400

    speed up lifter

--- Macarena2018/src/main/java/frc/team281/robot/subsystems/LifterSubsystem.java
+++ Macarena2018/src/main/java/frc/team281/robot/subsystems/LifterSubsystem.java
@@ -16,13 +16,13 @@ public class LifterSubsystem extends BaseSubsystem {
     private TalonSpeedController motorTwoController;
 
     public static final double CURRENT_STOP_FACTOR = 1.15;
-    public static final double UP_SPEED_PERCENT = 0.7;
-    public static final double DOWN_SPEED_PERCENT = 0.55;
+    public static final double UP_SPEED_PERCENT = 0.85;
+    public static final double DOWN_SPEED_PERCENT = 0.60;
     public static final double SLOW_SPEED_UP_PERCENT = 0.4;
     public static final double SLOW_SPEED_DOWN_PERCENT = 0.35;
     public static final int MIN_HEIGHT_COUNTS = 0;
     public static final int MAX_HEIGHT_COUNTS = 4268;
-    public static final int RAMP_DOWN_COUNTS = 300;
+    public static final int RAMP_DOWN_COUNTS = 600;
 
     private DigitalInput bottomLimitSwitch;
     private DigitalInput topLimitSwitch;
